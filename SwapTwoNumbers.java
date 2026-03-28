e8b2e3922a32d40d3b941bdd1dd7fae5b95a7628
AmrutaMudhol <amrutashokmudhol24@gmail.com>
2026-03-28 21:33:35 +0530

    apti

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,20 @@
+class SwapTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int a = 17;
+        int b = 18;
+
+        System.out.println("Before Swapping");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After Swapping");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
