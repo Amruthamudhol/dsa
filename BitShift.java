d0c051e9fae4ce32a360a7468ddfb57f4f0931d6
AmrutaMudhol <amrutashokmudhol24@gmail.com>
2026-03-29 22:18:32 +0530

    apti

---
+++ BitShift.java
@@ -0,0 +1,16 @@
+class BitShift {
+
+    public static void main(String[] args) 
+	{
+
+        int num = 11;
+
+        System.out.println("Original number: " + num);
+
+        int leftShift = num << 2;
+        System.out.println("After left shift (num << 2): " + leftShift);
+
+        int rightShift = num >> 2;
+        System.out.println("After right shift (num >> 2): " + rightShift);
+    }
+}
