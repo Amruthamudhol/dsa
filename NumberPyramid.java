9a83de78a7035ddf9847f61b5c4ed2de03e9391a
Amruthamudhol <amrutashokmudhol24@gmail.com>
2026-07-24 18:43:33 +0530

    number pattern

---
+++ NumberPyramid.java
@@ -0,0 +1,21 @@
+public class NumberPyramid {
+    public static void main(String[] args) {
+
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+
+            // Print spaces
+            for (int j = i; j < rows; j++) {
+                System.out.print(" ");
+            }
+
+            // Print numbers
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
