a547fb74cfb0182fabb64950b6159a874202ba98
Amruthamudhol <amrutashokmudhol24@gmail.com>
2026-08-01 19:44:41 +0530

    AlphabetPattern

---
+++ AlphabetPattern.java
@@ -0,0 +1,15 @@
+public class AlphabetPattern {
+    public static void main(String[] args) {
+
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+
+            for (char ch = 'A'; ch < 'A' + i; ch++) {
+                System.out.print(ch + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
